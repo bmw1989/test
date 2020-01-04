e03f1026d96b3687244d66f7d4c4010b1b51084c
BerradaMohamed <m.mohamed.berradad@gmail.com>
2020-01-04 17:29:51 +0100

    MAJ 04122020

--- src/app/pages/contribuable/fiche-contribuable/fiche-contribuable.component.ts
+++ src/app/pages/contribuable/fiche-contribuable/fiche-contribuable.component.ts
@@ -40,6 +40,8 @@ export class FicheContribuableComponent implements OnInit {
     settings: any;
 
     //Maps attributes
+    latitude= 18.088423;
+    longitude= -15.976214;
     options;
     streetMaps;
     wMaps;
@@ -52,6 +54,7 @@ export class FicheContribuableComponent implements OnInit {
     civilites = Civilite;
     civiliteSelected= "Mr";
 
+
     constructor(private authServiceApp: AuthenticationService,
                 private activiteService:ActiviteService,
                 private contribuableService:ContribuableService,
@@ -61,8 +64,11 @@ export class FicheContribuableComponent implements OnInit {
       this.newContribuable = new PersonnePhysique();
 
       this.newContribuable.civilite = 'Mr';
-      this.newContribuable.latitude= 46.879966;
-      this.newContribuable.longitude = -121.726909;
+
+      this.latitude= 18.088423;
+      this.longitude= -15.976214;
+
+
       this.getListTypeActivitePrincipal();
 
       this.initTableSettings();
@@ -106,111 +112,89 @@ export class FicheContribuableComponent implements OnInit {
     initMaps(){
 
       // Define our base layers so we can reference them multiple times
+
       this.streetMaps = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         detectRetina: true,
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       });
-      this.wMaps = L.tileLayer('http://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
-        detectRetina: true,
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      });
 
-      // Marker for the top of Mt. Ranier
-      this.summit = L.marker([ 46.8523, -121.7603 ], {
+      this.summit = L.marker([ this.latitude, this.longitude ], {
         icon: L.icon({
           iconSize: [ 25, 41 ],
           iconAnchor: [ 13, 41 ],
           iconUrl: 'leaflet/marker-icon.png',
           shadowUrl: 'leaflet/marker-shadow.png',
-        }),
+        }), draggable: true, autoPan: true,
+      }).on('dragend', function(event) {
+        const mark = event.target;
+        const coord = String(mark.getLatLng()).split(',');
+        //console.log(coord);
+        const lat = coord[0].split('(');
+
+        const lng = coord[1].split(')');
+
+        this.latitude = lat[1];
+        this.longitude = lng[0];
+
+        (<HTMLInputElement>window.document.getElementById("inputlartitude")).value = this.latitude;
+        (<HTMLInputElement>window.document.getElementById("inputlongitude")).value = this.longitude;
+
+        mark.bindPopup("Moved to: " + lat[1] + ", " + lng[0] + ".").update();
       });
-
-      // Marker for the parking lot at the base of Mt. Ranier trails
-      this.paradise = L.marker([ 46.78465227596462,-121.74141269177198 ], {
-        icon: L.icon({
-          iconSize: [ 25, 41 ],
-          iconAnchor: [ 13, 41 ],
-          iconUrl: 'leaflet/marker-icon.png',
-          shadowUrl: 'leaflet/marker-shadow.png',
-        }),
-      });
-
-      // Path from paradise to summit - most points omitted from this example for brevity
-      this.route = L.polyline([[ 46.78465227596462,-121.74141269177198 ],
-        [ 46.80047278292477, -121.73470708541572 ],
-        [ 46.815471360459924, -121.72521826811135 ],
-        [ 46.8360239546746, -121.7323131300509 ],
-        [ 46.844306448474526, -121.73327445052564 ],
-        [ 46.84979408048093, -121.74325201660395 ],
-        [ 46.853193528950214, -121.74823296256363 ],
-        [ 46.85322881676257, -121.74843915738165 ],
-        [ 46.85119913890958, -121.7519719619304 ],
-        [ 46.85103829018772, -121.7542376741767 ],
-        [ 46.85101557523012, -121.75431755371392 ],
-        [ 46.85140013694763, -121.75727385096252 ],
-        [ 46.8525277543813, -121.75995212048292 ],
-        [ 46.85290292836726, -121.76049157977104 ],
-        [ 46.8528160918504, -121.76042997278273 ]]);
-
-      // Layers control object with our two base layers and the three overlay layers
-      this.layersControl = {
-        baseLayers: {
-          'Street Maps': this.streetMaps,
-          'Wikimedia Maps': this.wMaps,
-        },
-        overlays: {
-          'Mt. Rainier Summit': this.summit,
-          'Mt. Rainier Paradise Start': this.paradise,
-          'Mt. Rainier Climb Route': this.route,
-        },
-      };
 
       this.options = {
-
         layers: [
-          this.streetMaps, this.route, this.summit, this.paradise,
+          this.streetMaps, this.summit, //this.route, this.summit, this.paradise,
         ],
-        zoom: 7,
-        center: L.latLng({ lat: this.newContribuable.latitude, lng: this.newContribuable.longitude }),
-
+        zoom: 13,
+        center: L.latLng({ lat: this.latitude, lng: this.longitude }),
       };
     }
 
+    private updateLatLng(e){
+      this.latitude = e.latlng.lat;
+      this.longitude = e.latlng.lng;
+    }
   mapReady(map: L.Map) {
-    map.addControl(L.control.zoom({position: 'bottomright'}));
+   /* map.addControl(L.control.zoom({position: 'bottomright'}));
 
     map.fitBounds(this.route.getBounds(), {
       padding: L.point(24, 24),
       maxZoom: 12,
       animate: true,
-    });
+    });*/
 
     // fix the map fully displaying, existing leaflet bag
     setTimeout(() => {
       map.invalidateSize();
-    }, 12000);
+    }, 8000);
   }
-    ajouterContribuable(){
-      const listActivites : FicheActivite[]= [];
-      for(const typeActv of this.listActivitesChoisies){
-        
-        const fiche:FicheActivite = new FicheActivite();
 
-        fiche.refActivite = typeActv;
-        listActivites.push(fiche);
-      }
-      this.newContribuable.listActivite = listActivites;
+    ajouterContribuable():void {
+      if (this.isValidContribuable()) {
+        const listActivites: FicheActivite[] = [];
+        for (const typeActv of this.listActivitesChoisies) {
 
-      this.contribuableService.ajouterNouveauPersonnePhysique(this.newContribuable).then(resultat => {
-        //morphoPere = resultat.data as MorphoPersonne;
-        this.resultVO = resultat;
-      }, (error => {
-        this.resultVO = error;
-        this.initializeResultVO();
-        if (this.resultVO.isDeconnected) {
-          this.authServiceApp.logoutWithParam();
+          const fiche: FicheActivite = new FicheActivite();
+
+          fiche.refActivite = typeActv;
+          listActivites.push(fiche);
         }
+        this.newContribuable.listActivite = listActivites;
+
+        this.newContribuable.latitude= this.latitude;
+        this.newContribuable.longitude = this.longitude;
+        this.contribuableService.ajouterNouveauPersonnePhysique(this.newContribuable).then(resultat => {
+          //morphoPere = resultat.data as MorphoPersonne;
+          this.resultVO = resultat;
+        }, (error => {
+          this.resultVO = error;
+          this.initializeResultVO();
+          if (this.resultVO.isDeconnected) {
+            this.authServiceApp.logoutWithParam();
+          }
         }));
+      }
     }
 
 	  ajouterActivite(){
@@ -225,8 +209,24 @@ export class FicheContribuableComponent implements OnInit {
     }
 
 
-    isValidContribuable(){
+    private isValidContribuable():boolean{
 
+      if(this.newContribuable.nni == null || this.newContribuable.nni === ''){
+        this.resultVO.messagesErrors.push(this.getMessage('MSG_ERR.CONTRIBUABLE.MSG_ERR_CONT_002'));
+      }
+      if(this.newContribuable.nomFr == null || this.newContribuable.nomFr === ''){
+        this.resultVO.messagesErrors.push(this.getMessage('MSG_ERR.CONTRIBUABLE.MSG_ERR_CONT_003'));
+      }
+      if(this.newContribuable.prenomFr == null || this.newContribuable.prenomFr === ''){
+        this.resultVO.messagesErrors.push(this.getMessage('MSG_ERR.CONTRIBUABLE.MSG_ERR_CONT_004'));
+      }
+
+      if( this.resultVO.messagesErrors.length>0){
+        this.initializeResultVO();
+        return false;
+      }
+
+      return true;
     }
 
     validationActivite(){
@@ -246,9 +246,12 @@ export class FicheContribuableComponent implements OnInit {
 
     }
 
-    getMessage(message, val){
+    private getMessage(message:string, val?):string{
+      if(val == null)
+        return this.translate.instant(message);
       return this.translate.instant(message, {value: val});
     }
+
 
     changeActivitePrincipal(typeActPrincipale){
       this.activitePrincipalSelected = typeActPrincipale;
